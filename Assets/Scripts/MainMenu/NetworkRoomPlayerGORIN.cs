568c951f4e43b6bd998bcdc8b125171743c003e6
thomas <thomas.hebert2020@gmail.com>
2022-10-21 14:54:43 +0200

    Working on MP lobby

--- Assets/Scripts/MainMenu/NetworkRoomPlayerGORIN.cs
+++ Assets/Scripts/MainMenu/NetworkRoomPlayerGORIN.cs
@@ -13,14 +13,76 @@ public class NetworkRoomPlayerGORIN : NetworkBehaviour
    [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[2];
    [SerializeField] private Button startGameButton = null;
 
-   //[SyncVar(hook = nameof(HandleDisplayNameChanged))]
+   [SyncVar(hook = nameof(HandleDisplayNameChanged))]
    public string DisplayName;
 
-   //[SyncVar(hook = nameof(HandleReadyStatusChange))]
+   [SyncVar(hook = nameof(HandleReadyStatusChange))]
    public bool IsReady = false;
 
-   private bool isLeader;
+   private bool _isLeader;
 
-  // public bool IsLeader() {}
-  //6:18
+   public bool IsLeader
+   {
+      set
+      {
+         _isLeader = value;
+         startGameButton.gameObject.SetActive(value);
+      }
+   }
+
+   private NetworkManagerGORIN room;
+
+   private NetworkManagerGORIN Room
+   {
+      get
+      {
+         if (room != null) { return room; }
+         return room = NetworkManager.singleton as NetworkManagerGORIN;
+      }
+   }
+
+   public override void OnStartAuthority()
+   {
+      CmdSetDisplayName(PlayerNameInput.DisplayName);
+      lobbyUI.SetActive(true);
+   }
+
+   public override void OnStartClient()
+   {
+      Room.RoomPlayers.Add(this);
+      UpdateDisplay();
+   }
+
+   public override void OnStopClient()
+   {
+      Room.RoomPlayers.Remove(this);
+
+      UpdateDisplay();
+   }
+
+   public void HandleReadyStatusChange(bool oldValue, bool newValue) => UpdateDisplay();
+
+   public void HandleDisplayNameChanged(string oldValue, string newValue) => UpdateDisplay();
+
+   private void UpdateDisplay()
+   {
+      if (!isLocalPlayer)
+      {
+         foreach (var player in Room.RoomPlayers)
+         {
+            if (player.isLocalPlayer)
+            {
+               player.UpdateDisplay();
+               break;
+            }
+         }
+         return;
+      }
+
+      for (int i = 0; i < playerNameTexts.Length; i++)
+      {
+         
+      }
+   }
+   //9:42
 }
