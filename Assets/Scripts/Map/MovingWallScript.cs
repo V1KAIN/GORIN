8f6453a69d3ade28eb2c0ce2bb22dee5d30765dd
VIKAIN <thomas.hebert2020@gmail.com>
2022-12-02 11:44:02 +0100

    <>
    +Move wall now worp

--- Assets/Scripts/Map/MovingWallScript.cs
+++ Assets/Scripts/Map/MovingWallScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,12 @@ public class MovingWallScript : MonoBehaviour
     public bool _isWallUp;
     [SerializeField] private Animator _wallAnimator;
     [SerializeField] private GameObject _movingWallManager;
-    
+
+    private void Start()
+    {
+        _movingWallManager.GetComponent<MovingWallsManager>().MovingWallsInScene.Add(gameObject);
+    }
+
     public IEnumerator SetWallUp()
     {
         _isWallUp = false;
